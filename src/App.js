64e690717fcb2f57c8ca65587b2d75e64fcc9744
satasiyaprince <princesatasiya49@gmail.com>
2022-08-22 08:59:08 +0530

    master+

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import './App.css';
 import Header from './Component/Header/Header';
 import Footer from './Component/Footer/Footer';
 import Home from './Container/Home/Home';
-import About from './Container/Home/About/About';
 import Appointment from './Container/Appointment/Appointment';
 import Contact from './Container/Contact/Contact';
 import Departments from './Container/Departments/Departments';
 import Doctors from './Container/Doctors/Doctors';
+import About from './Container/About/About';
+import {Route , Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Header />
-      {/* <Home /> */}
-      {/* <About /> */}
-      {/* <Appointment /> */}
-      {/* <Contact /> */}
-      <Departments />
-      {/* <Doctors /> */}
+    <Switch>
+      <Route exact path={"/"} component={Home}/>
+      <Route exact path={"/departments"} component={Departments}/>
+      <Route exact path={"/about"} component={About}/>
+      <Route exact path={"/doctors"} component={Doctors}/>
+      <Route exact path={"/appointment"} component={Appointment}/>
+      <Route exact path={"/contact"} component={Contact}/>
+    </Switch>
       <Footer />
     </div>
   );
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Header(props) {
     return (
@@ -27,16 +28,16 @@ function Header(props) {
                     </div>
                     <nav id="navbar" className="navbar order-last order-lg-0">
                         <ul>
-                            <li><a className="nav-link scrollto active" href="index.html">Home</a></li>
-                            <li><a className="nav-link scrollto" href="./pages/departments.html">Departments</a></li>
-                            <li><a className="nav-link scrollto" href="./pages/doctors.html">Doctors</a></li>
-                            <li><a className="nav-link scrollto " href="./pages/about.html">About</a></li>
-                            <li><a className="nav-link scrollto" href="./pages/contact.html">Contact</a></li>
+                            <li><NavLink className="nav-link scrollto active" to={"/"}>Home</NavLink></li>
+                            <li><NavLink className="nav-link scrollto" to={"/departments"}>Departments</NavLink></li>
+                            <li><NavLink className="nav-link scrollto" to={"/doctors"}>Doctors</NavLink></li>
+                            <li><NavLink className="nav-link scrollto " to={"/about"}>About</NavLink></li>
+                            <li><NavLink className="nav-link scrollto" to={"/contact"}>Contact</NavLink></li>
                         </ul>
                         <i className="bi bi-list mobile-nav-toggle" />
                     </nav>
-                    <a href="./pages/appointment.html" className="appointment-btn scrollto"><span className="d-none d-md-inline">Make an</span>
-                        Appointment</a>
+                    <NavLink to={"/appointment"} className="appointment-btn scrollto"><span className="d-none d-md-inline">Make an</span>
+                        Appointment</NavLink>
                     <a href="#" className="appointment-btn scrollto">
                         <span className="d-none d-md-inline">Login/ Signup</span>
                     </a>
