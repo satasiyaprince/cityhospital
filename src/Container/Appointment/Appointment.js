15937a3f26d40b8009cb4f7f976c5e3d873b7076
satasiyaprince <princesatasiya49@gmail.com>
2022-10-09 21:43:22 +0530

    Usehistory

--- src/Container/Appointment/Appointment.js
+++ src/Container/Appointment/Appointment.js
@@ -10,14 +10,36 @@ function Appointment(props) {
 
     let appschema, initc;
 
+    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
+
     appschema = {
-        name: yup.string().required("Enter your name"),
-        email: yup.string().required("Enter your email"),
-        phone: yup.string().required("Enter your phone number").min(10),
-        date: yup.string().required("Enter your appointment date"),
+        name: yup.string().required("Enter your name")
+            .min(2, "Mininum 2 characters")
+            .max(30, "Maximum 30 characters")
+            .matches(
+                /^([A-Za-z\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff\s]*)$/gi,
+                'Enter valid name'
+            ),
+        email: yup.string().required("Enter your email").email("Enter valid email"),
+
+        phone: yup.string().required("Enter your phone number")
+            .matches(phoneRegExp, 'Phone number is not valid')
+            .min(10, 'Enter min 10 digits')
+            .max(10, 'Enter max 10 digits'),
+
+        date: yup.string().required("Enter your appointment date")
+            .matches(/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/, "Enter Date in Format."),
+
         department: yup.string().required("Enter your Select Department"),
-        message: yup.string().min(6).required("Enter your message"),
+
+        message: yup.string().required("Enter your message")
+            .min(2, 'minimum required 2 diget')
+            .max(50, 'maximum required 50 diget')
+            .required('Enter your messge'),
+
         gender: yup.string().required("Enter your Gender"),
+        
         checkbox: yup.array().min(1).of(yup.string().required()).required("select your Hobby"),
 
 
@@ -35,16 +57,16 @@ function Appointment(props) {
 
     }
 
-    const handleAdd = (values) =>{
+    const handleAdd = (values) => {
 
         let localData = JSON.parse(localStorage.getItem("apt"));
         console.log(localData);
 
-        if(localData === null){
-            localStorage.setItem ("apt",JSON.stringify([values]));
-        }else{
+        if (localData === null) {
+            localStorage.setItem("apt", JSON.stringify([values]));
+        } else {
             localData.push(values);
-            localStorage.setItem ("apt" , JSON.stringify(localData));
+            localStorage.setItem("apt", JSON.stringify(localData));
         }
 
         history.push("/listappointment");
